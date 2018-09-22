4c7ae0543295d05eccbe4a9e57ea316cfb4e783a
GarimaSingh1997 <42217662+GarimaSingh1997@users.noreply.github.com>
2018-09-22 23:15:58 +0530

    division

---
+++ DIV.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ printf("Enter first number then second number after entering first number");
+ scanf("%d%d",&a,&b);
+ c=a/b;
+ printf("Result is %d",c);
+ getch();
+}
